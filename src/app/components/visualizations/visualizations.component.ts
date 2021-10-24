62d70d917a199fb3cd49a857dee1a3c6e36a8b03
sai-m021 <sai.manchikalapati@gmail.com>
2021-10-24 01:09:49 -0400

    .

--- src/app/components/visualizations/visualizations.component.ts
+++ src/app/components/visualizations/visualizations.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-visualizations',
   templateUrl: './visualizations.component.html',
   styleUrls: ['./visualizations.component.scss']
 })
 export class VisualizationsComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
